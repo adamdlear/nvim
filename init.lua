bd198fbed53b7bd4175e82e044dd73a7989311a0
Adam Lear <adamlear12@gmail.com>
2024-01-26 13:14:16 -0500

    add lualine

--- init.lua
+++ init.lua
@@ -1,9 +1,3 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.g.mapleader = " "
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -19,6 +13,8 @@ vim.opt.rtp:prepend(lazypath)
 
 local opts = {}
 
+require("vim-options")
 require("lazy").setup("plugins")
 
-vim.keymap.set('n', '<leader>l', 'Neotree filesystem reveal left<CR>', {})
+-- Keymaps
+vim.keymap.set("i", "jk", "<Esc>", {})
